cb0e06e4ace2551a3e06d403847a74a29af40f46
Renann Santos <69852026+Renann1@users.noreply.github.com>
2026-03-11 19:04:50 -0300

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,3 +1,2 @@
-// Preencha com os dados do seu projeto Supabase
-window.SUPABASE_URL = "https://ctkyoinpzlzlyhynzap.supabase.co";
-window.SUPABASE_ANON_KEY = "sb_publishable_B0nL3IRnWg7mbdEoyYjrOA_fLmtcpLP";
+window.SUPABASE_URL = "https://ctkyoinplzlzyhyznapl.supabase.co";
+window.SUPABASE_ANON_KEY = "sb_publishable_B0nL3IRNwg7mbdEoyYjr0A_fLmtcpLP";
