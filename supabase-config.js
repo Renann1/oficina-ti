d2b1e302f9d1ef0a3415ff398652881229838af8
Renann Santos <69852026+Renann1@users.noreply.github.com>
2026-03-11 19:22:42 -0300

    Update2 supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,2 +1,2 @@
 window.SUPABASE_URL = "https://ctkyoinplzlzyhyznapl.supabase.co";
-window.SUPABASE_ANON_KEY = "sb_publishable_B0nL3IRNwg7mbdEoyYjr0A_fLmtcpLP";
+window.SUPABASE_ANON_KEY = "sb_publishable_kjkrB-NNoP5phce9cK60tA_n5O2APPJ";
